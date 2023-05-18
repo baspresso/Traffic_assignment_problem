be60299537d28ce401c565ec2c11462241305094
baspresso <80454286+baspresso@users.noreply.github.com>
2023-05-18 21:39:42 +0300

    Update solution_and_tests.cpp

--- tap_parallel_computing/solution_and_tests.cpp
+++ tap_parallel_computing/solution_and_tests.cpp
@@ -22,25 +22,25 @@ using namespace std;
 #include <Eigen/Dense>
 #include "tap_approaches.h"
 
-using dec101_t = math::wide_decimal::decwide_t<INT32_C(101), std::uint32_t, void>;
+using dec101_t = math::wide_decimal::decwide_t<INT32_C(50), std::uint32_t, void>;
 
 template <typename Method>
 void TrafficAssignmentTest(string test) {
+  auto start = chrono::high_resolution_clock::now();
+  cout << "test: " << test << '\n';
   Method tap_test(test);
   tap_test.SolveFlow();
+  auto end = chrono::high_resolution_clock::now();
+  chrono::duration<float> duration = end - start;
+  cout << "Duration: " << duration.count() << " sec \n";
   //tap_test.ShowStatistics();
 }
 void TrafficAssignmentTest_solutions() {
   string line;
   ifstream in("tests.txt");
   while (getline(in, line)) {
-    auto start = chrono::high_resolution_clock::now();
-    cout << "test: " << line << '\n';
-    //TrafficAssignmentTest<tap_tr_equilibrium>(line);
+    TrafficAssignmentTest<traffic_assignment::LinkBasedApproach <dec101_t>>(line);
     TrafficAssignmentTest<traffic_assignment::RouteBasedApproach <dec101_t>>(line);
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<float> duration = end - start;
-    cout << "Duration: " << duration.count() << " sec \n";
   }
 }
 
